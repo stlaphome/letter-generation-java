d1c2cd74d267c9572d1686834647a6c49bebb584
gokila <gokila.g@ivtlinfoview.co.jp>
2023-11-19 11:01:48 +0530

    commit for update letter

--- src/main/java/com/st/lap/dynamicReportTemplate/model/LetterProduct.java
+++ src/main/java/com/st/lap/dynamicReportTemplate/model/LetterProduct.java
@@ -15,19 +15,19 @@ import lombok.Data;
 public class LetterProduct {
 	
 	@Id
-	@Column(name = "dynamic_report_container_hdr_key")
-	private int ProductId;
+	@Column(name = "product_id")
+	private int productId;
 	
-	@Column(name = "report_file_name")
+	@Column(name = "product_code")
 	private String productCode;
 	
-	@Column(name = "report_file")
+	@Column(name = "letter_name")
 	private Blob letterName;
 	
-	@Column(name = "report_file")
+	@Column(name = "data_base")
 	private Blob dataBase;
 	
-	@Column(name = "report_file")
+	@Column(name = "product_data")
 	private Blob productData;
 
 
